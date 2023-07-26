554269ef73aa7425f32f27b5b82da63baadf80b5
pabnoob1 <parthbhatt2249@gmail.com>
2023-07-26 13:16:44 +0530

    Updates

---
+++ precache-manifest.92eeef64139a163cd800bac5b51517fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "047a3a3d7eea93e4cc73",
+    "url": "/TextUtils/static/css/main.886136f6.chunk.css"
+  },
+  {
+    "revision": "047a3a3d7eea93e4cc73",
+    "url": "/TextUtils/static/js/main.047a3a3d.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/TextUtils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "df05ce19991abad81e6f",
+    "url": "/TextUtils/static/js/runtime~main.df05ce19.js"
+  },
+  {
+    "revision": "36188e0838254abbb6794efda904901f",
+    "url": "/TextUtils/index.html"
+  }
+];
